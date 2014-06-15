23c24434e818b875e764a45c8fedfebf1b5ec086
Toomore Chiang <toomore0929@gmail.com>
2014-06-15 10:11:56 +0800

    Add tour_13.

---
+++ tour_13.go
@@ -0,0 +1,11 @@
+//http://tour.golang.org/#12
+package main
+
+import "fmt"
+
+
+func main() {
+    i, j := 1, 2
+    c, python, java := true, false, "toomore"
+    fmt.Println(i, j, c, python, java)
+}
