df931974e4794bc8939ef4ee48c85f316b7459a5
Toomore Chiang <toomore0929@gmail.com>
2014-06-15 10:22:20 +0800

    Add some diff syntax but the same result.

--- tour_15.go
+++ tour_15.go
@@ -5,8 +5,11 @@ import "fmt"
 import "math"
 
 func main() {
-    var x, y int = 3, 4
-    var f float64 = math.Sqrt(float64(3*3 + 4*4))
-    var z int = int(f)
+    //var x, y int = 3, 4
+    var x, y = 3, 4
+    //var f float64 = math.Sqrt(float64(3*3 + 4*4))
+    var f = math.Sqrt(float64(3*3 + 4*4))
+    //var z int = int(f)
+    var z = int(f)
     fmt.Println(x, y, z)
 }
