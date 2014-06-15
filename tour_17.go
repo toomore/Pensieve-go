e9add61bb8f616b45efb0dbe62ae95e34027b07b
Toomore Chiang <toomore0929@gmail.com>
2014-06-15 11:55:15 +0800

    Add tour_17.

---
+++ tour_17.go
@@ -0,0 +1,22 @@
+//http://tour.golang.org/#17
+package main
+
+import "fmt"
+
+const Big = 1 << 100
+const Small = Big >> 99
+
+func needInt(x int) int {
+    return x * 10 + 1
+}
+
+func needFloat(x float64) float64 {
+    return x * 0.1
+}
+
+func main() {
+    fmt.Println(Small)
+    fmt.Println(needInt(Small))
+    fmt.Println(needFloat(Small))
+    fmt.Println(needFloat(Big))
+}
