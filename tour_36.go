53a9a4cf3bb49f15eef815e704928d8af7e3d3c9
Toomore Chiang <toomore0929@gmail.com>
2014-06-17 08:33:27 +0800

    Add tour_36.

---
+++ tour_36.go
@@ -0,0 +1,13 @@
+//http://tour.golang.org/#36
+package main
+
+import "fmt"
+
+var pow = []int{1, 2, 3, 5, 8, 11, 13}
+
+func main() {
+    fmt.Println(pow)
+    for i, v := range pow {
+        fmt.Println(i, v)
+    }
+}
