593c079e0eba679b600c604d41a8c5e50d381a1f
Toomore Chiang <toomore0929@gmail.com>
2014-06-17 08:46:40 +0800

    Tiny changed in tour_36.

--- tour_36.go
+++ tour_36.go
@@ -8,6 +8,6 @@ var pow = []int{1, 2, 3, 5, 8, 11, 13}
 func main() {
     fmt.Println(pow)
     for i, v := range pow {
-        fmt.Println(i, v)
+        fmt.Printf("2**%d = %d\n", i, v)
     }
 }
