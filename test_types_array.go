0403514c8425853865af12daea61c7a50deccbc0
Toomore Chiang <toomore0929@gmail.com>
2014-06-20 00:41:14 +0800

    Add append.

--- test_types_array.go
+++ test_types_array.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
     x := make([]int8, 5 ,10)
-    x[1] = 1
+    //var x []int
     fmt.Println(len(x))
     fmt.Println(cap(x))
-    fmt.Println(x[6:9])
+    //fmt.Println(x[6:9])
+    y := append(x, 1)
+    fmt.Println(y)
 }
