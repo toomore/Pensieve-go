13a107ade9f8deae80a3b51d419bff30aba32e34
Toomore Chiang <toomore0929@gmail.com>
2014-06-21 23:07:59 +0800

    Add sum, average into example_average.

--- example_average.go
+++ example_average.go
@@ -5,14 +5,21 @@ import "fmt"
 func main() {
     fmt.Println("average")
     x := []int8{1, 2, 3, 4, 5}
+    fmt.Println(sum(x))
     fmt.Println(average(x))
 }
 
-func average(list []int8) (total int8) {
+func sum(list []int8) (total int8) {
     //total = 0
     for _, v := range list {
         total += v
     }
     //return total
+    return
+}
+
+func average(list []int8) (average int8) {
+    total := sum(list)
+    average = total / int8(len(list))
     return
 }
