9283625cac89b33c35dc3822212a9b112e2c2874
Toomore Chiang <toomore0929@gmail.com>
2014-06-24 23:27:11 +0800

    Add func into example_structs.

--- example_structs.go
+++ example_structs.go
@@ -6,7 +6,14 @@ type Circle struct {
     x, y, z float64
 }
 
+func total(x *Circle) float64 {
+    return x.x + x.y + x.z
+}
+
 func main() {
-    c := new(Circle)
+    //c := new(Circle)
+    c := Circle{1, 2, 3}
+
     fmt.Println(c)
+    fmt.Println(total(&c))
 }
