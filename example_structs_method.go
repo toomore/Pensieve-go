6c5dc65db7cc436a4b5f3d5028bcee4f37381918
Toomore Chiang <toomore0929@gmail.com>
2014-06-24 23:57:15 +0800

    Add example_structs_method.

---
+++ example_structs_method.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Circle struct {
+    x, y, z float64
+}
+
+func (x *Circle) total() float64 {
+    return x.x + x.y + x.z
+}
+
+func main() {
+    //c := new(Circle)
+    c := Circle{1, 2, 3}
+
+    fmt.Println(c)
+    fmt.Println(c.total())
+}
