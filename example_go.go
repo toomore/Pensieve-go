1a91d580510bc57875ae79281161c3867d4cba07
Toomore Chiang <toomore0929@gmail.com>
2014-06-25 23:51:54 +0800

    Add more "go".

--- example_go.go
+++ example_go.go
@@ -1,15 +1,21 @@
 package main
 
 import "fmt"
+import "time"
+import "math/rand"
 
 func f(n int) {
     for i := 0; i < n; i++ {
         fmt.Println(i)
+        amt := time.Duration(rand.Intn(550))
+        time.Sleep(time.Millisecond * amt)
     }
 }
 
 func main() {
-    go f(10)
+    for i :=0; i < 10; i++ {
+        go f(10)
+    }
     var input string
     fmt.Scanln(&input)
 }
