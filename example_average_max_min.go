45d41b3baf6b2477408da6d668624698191d3b58
Toomore Chiang <toomore0929@gmail.com>
2014-06-27 22:51:14 +0800

    Add max.

--- example_average_max_min.go
+++ example_average_max_min.go
@@ -13,10 +13,21 @@ func sum(args ...float64) (total float64) {
     return
 }
 
+func max(args ...float64) (max float64) {
+    max = args[0]
+    for _, v := range args[1:] {
+        if v > max {
+            max = v
+        }
+    }
+    return
+}
+
 func main() {
     float64list := []float64{1, 2, 3, 4, 5}
 
     //http://golang.org/ref/spec#Passing_arguments_to_..._parameters
-    fmt.Println(sum(float64list...))
-    fmt.Println(average(float64list...))
+    fmt.Println("SUM", sum(float64list...))
+    fmt.Println("AVERAGE", average(float64list...))
+    fmt.Println("MAX", max(float64list...))
 }
