8bcf2019651ed3bb7762b65032fc026a08c0d39f
Toomore Chiang <toomore0929@gmail.com>
2014-06-27 22:47:02 +0800

    Add sum.

--- example_average_max_min.go
+++ example_average_max_min.go
@@ -3,16 +3,20 @@ package main
 import "fmt"
 
 func average(args ...float64) float64 {
-    var total float64
+    return sum(args...) / float64(len(args))
+}
+
+func sum(args ...float64) (total float64) {
     for _, v := range args {
         total += v
     }
-    return total / float64(len(args))
+    return
 }
 
 func main() {
     float64list := []float64{1, 2, 3, 4, 5}
 
     //http://golang.org/ref/spec#Passing_arguments_to_..._parameters
+    fmt.Println(sum(float64list...))
     fmt.Println(average(float64list...))
 }
