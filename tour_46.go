0d308fa1f8b5c823118991f4c17d1803d9d30bc8
Toomore Chiang <toomore0929@gmail.com>
2014-06-29 16:15:55 +0800

    Add tour_46.

---
+++ tour_46.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func fibonacci() func(int) int {
+    a := 0
+    b := 1
+    return func(x int) (result int) {
+        if x > 1 {
+            result = a + b
+            a, b = b, result
+        }
+        return
+    }
+}
+
+func main() {
+    fi := fibonacci()
+    for i := 0; i < 10; i++ {
+        fmt.Println(i, fi(i))
+    }
+}
+
+//0: 0
+//1: 1
+//2: 0 + 1
+//3: 1 + 0 + 1
+//4: 0 + 1 + 1 + 0 + 1
