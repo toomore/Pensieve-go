20e513dc5dbafbc5d35da07f90f3d556eca575dd
Toomore Chiang <toomore0929@gmail.com>
2014-07-02 08:57:41 +0800

    Test pointer.

--- test_type_pointer.go
+++ test_type_pointer.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    a := 50
-    var b *int
-    b = &a
-    fmt.Println(a, b)
+    //a := 50
+    //var b *int
+    //b = &a
+    //fmt.Println(a, b)
+    var a uint8 = 40
+    var b *uint8 = &a
+    fmt.Println(a, &a, b)
+    *b = 255
+    fmt.Println(a, &a, b)
 }
