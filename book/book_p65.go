4095ccd6afbb85f50a87c388a94cc5ce30d1e7cb
Toomore Chiang <toomore0929@gmail.com>
2014-07-09 01:13:19 +0800

    Add book/book_p65.go.

---
+++ book/book_p65.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+    fmt.Println(math.Ceil(5.6))
+    fmt.Println(math.Floor(5.6))
+    fmt.Println(math.Pow(2, 3))
+    fmt.Println(math.MaxUint8)
+    fmt.Println(math.MaxUint32)
+}
