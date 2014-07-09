c2d118f5d3210f9ed7dccd252f259837628c4752
Toomore Chiang <toomore0929@gmail.com>
2014-07-09 08:05:24 +0800

    Add median.

--- book/book_p73.go
+++ book/book_p73.go
@@ -1,6 +1,8 @@
 package main
 
 import "fmt"
+import "math"
+import "sort"
 
 type statistics struct {
     numbers []float64
@@ -19,9 +21,16 @@ func Average(numbers []float64) float64 {
     return Sum(numbers) / float64(len(numbers))
 }
 
+func Median(numbers []float64) float64 {
+    sort.Float64s(numbers)
+    return numbers[int(math.Ceil(float64(len(numbers)) / 2))-1]
+}
+
 func getStatis(numbers []float64) (stats statistics) {
     stats.numbers = numbers
+    sort.Float64s(stats.numbers)
     stats.mean = Average(numbers)
+    stats.median = Median(numbers)
     return
 }
 
