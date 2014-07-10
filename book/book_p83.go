6c9fb83166db46aa31efe5639c99b3d9665bbb4c
Toomore Chiang <toomore0929@gmail.com>
2014-07-11 07:58:09 +0800

    Add test string format.

--- book/book_p83.go
+++ book/book_p83.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "strconv"
+import "time"
 
 func main() {
     fmt.Println("123")
@@ -12,4 +13,10 @@ func main() {
     fmt.Println(strconv.FormatUint(123123123, 16))
     fmt.Println([]rune("a國"))
     fmt.Println([]byte("a國"))
+    tt := time.Now()
+    fmt.Println(tt.Nanosecond())
+
+    // string format
+    fmt.Printf("%v %v %v \n", "123", 500, "國")
+    fmt.Printf("%s %d %T \n", "123", 500, "國")
 }
