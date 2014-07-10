7a69948e5e0c6b463050b10f61e9a080d2888625
Toomore Chiang <toomore0929@gmail.com>
2014-07-11 01:21:06 +0800

    Test rune.

--- book/book_p83.go
+++ book/book_p83.go
@@ -1,10 +1,15 @@
 package main
 
 import "fmt"
+import "strconv"
 
 func main() {
     fmt.Println("123")
     fmt.Println("\u221E \U0000221a")
     fmt.Println("Z" > "b")
     fmt.Println([]byte("AaBbZz"))
+    fmt.Println(strconv.Itoa(123))
+    fmt.Println(strconv.FormatUint(123123123, 16))
+    fmt.Println([]rune("a國"))
+    fmt.Println([]byte("a國"))
 }
