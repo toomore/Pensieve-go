3b4e757d11efeae845ff58ad4679a9a8beb7378e
Toomore Chiang <toomore0929@gmail.com>
2014-07-12 13:55:55 +0800

    Add test book p108.

--- book/book_p83.go
+++ book/book_p83.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "strconv"
+import "strings"
 import "time"
 
 func main() {
@@ -11,12 +12,18 @@ func main() {
     fmt.Println([]byte("AaBbZz"))
     fmt.Println(strconv.Itoa(123))
     fmt.Println(strconv.FormatUint(123123123, 16))
-    fmt.Println([]rune("a國"))
-    fmt.Println([]byte("a國"))
+    fmt.Println([]rune("a功"))
+    fmt.Println([]byte("a功"))
     tt := time.Now()
     fmt.Println(tt.Nanosecond())
 
     // string format
     fmt.Printf("%v %v %v \n", "123", 500, "國")
     fmt.Printf("%s %d %T \n", "123", 500, "國")
+
+    // page 108
+    fmt.Println(strings.Contains("國家", "國"))
+    fmt.Println(strings.Repeat("功蓋許", 10))
+    fmt.Println(strings.Title("Toomore is me."))
+    fmt.Println(strings.TrimSpace(" I am Toomore! "))
 }
