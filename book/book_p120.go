3e2a5c019c65da3ab9e6ea5a72905f519db14ade
Toomore Chiang <toomore0929@gmail.com>
2014-07-15 21:36:05 +0800

    Add tiny test.

--- book/book_p120.go
+++ book/book_p120.go
@@ -2,8 +2,21 @@ package main
 
 import "regexp"
 import "fmt"
+import "strings"
+
+
+func MatchStringSplit(s string, reg *regexp.Regexp) {
+    for _, str := range strings.Split(s, ",") {
+        str = strings.TrimSpace(str)
+        fmt.Println(reg.String(), str, reg.MatchString(str))
+    }
+}
 
 func main() {
-    reg, _ := regexp.Compile(`^[a-z0-9\s]+$`)
+    reg := regexp.MustCompile(`^[a-z0-9\s]+$`)
     fmt.Println(reg.FindAllString("book 123", -1))
+    fmt.Println(reg.MatchString("book 123"))
+
+    reg2 := regexp.MustCompile(`^[[:word:]]+$`)
+    MatchStringSplit("toomore, toomore0929, 123, tomor!@#, too_more, A B", reg2)
 }
