6f321009f3fee13d672fb1966099a99f0e6e6bbb
Toomore Chiang <toomore0929@gmail.com>
2014-07-16 21:11:52 +0800

    Add ref type.

--- book/book_p146.go
+++ book/book_p146.go
@@ -9,9 +9,23 @@ func inflate(numbers []int, factor int) {
     }
 }
 
+type oo struct {
+    x, y, z int
+}
+
+func ooo(o *oo, x, y int) {
+    (*o).x += x
+    (*o).y += y
+}
+
 func main() {
     numbers := []int{1,2,3,4,5,6} //array pointer
     fmt.Println(&numbers)
     inflate(numbers, 3)
     fmt.Println(numbers)
+
+    o := oo{1, 2, 3}
+    fmt.Println(o)
+    ooo(&o, 2, 1) // ref type.
+    fmt.Println(o)
 }
