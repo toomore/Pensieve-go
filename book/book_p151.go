548a0d55554cce97d90b5c632c46cbe1cd9ce40c
Toomore Chiang <toomore0929@gmail.com>
2014-07-16 21:39:41 +0800

    Add slices with len, cap.

--- book/book_p151.go
+++ book/book_p151.go
@@ -7,4 +7,11 @@ func main() {
     fmt.Println(a, len(a), cap(a))
     a = a[:cap(a)] // len == cap
     fmt.Println(a, len(a), cap(a))
+
+    b := a[:6]
+    fmt.Println(b, len(b), cap(b)) // 5, 10
+    b = a[2:6]
+    fmt.Println(b, len(b), cap(b)) // 4, 8
+    b = a[9:]
+    fmt.Println(b, len(b), cap(b)) // 1, 1
 }
