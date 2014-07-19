beb8f27bdd38dc6addce500b84070822a96e9e9f
Toomore Chiang <toomore0929@gmail.com>
2014-07-19 23:26:39 +0800

    Add post.

--- test_net_http_2.go
+++ test_net_http_2.go
@@ -8,16 +8,23 @@ import "net/url"
 
 type params map[string]string
 
-func (p params) String() string {
+func (p params) String() url.Values {
     result := url.Values{}
     for key, value := range p {
         result.Add(key, value)
     }
-    return result.Encode()
+    return result
 }
 
 func httpGet(url string, params params) []byte {
-    resp, _ := http.Get(fmt.Sprintf("%s?%s", url, params))
+    resp, _ := http.Get(fmt.Sprintf("%s?%s", url, params.String().Encode()))
+    defer resp.Body.Close()
+    str, _ := ioutil.ReadAll(resp.Body)
+    return str
+}
+
+func httpPost(url string, params params) []byte {
+    resp, _ := http.PostForm(url, params.String())
     defer resp.Body.Close()
     str, _ := ioutil.ReadAll(resp.Body)
     return str
@@ -29,7 +36,12 @@ func main() {
     //v.Add("name", "toomore")
     //v.Add("age", "30")
     //fmt.Println(v.Encode())
+
+    // Get.
     p := params{"name": "toomore", "age": "30"}
     fmt.Println(p)
     fmt.Printf("%s\n", httpGet("http://httpbin.org/get", p))
+
+    // Post.
+    fmt.Printf("%s\n", httpPost("http://httpbin.org/post", p))
 }
