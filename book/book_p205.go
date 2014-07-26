92315f76a4cdf113e09d9521f69393e850cc51cb
Toomore Chiang <toomore0929@gmail.com>
2014-07-26 19:28:40 +0800

    Add meth/rand.

--- book/book_p205.go
+++ book/book_p205.go
@@ -1,11 +1,14 @@
 package main
 
 import "fmt"
+import "time"
+import "math/rand"
 
 func counter(start int) chan int {
-	a := make(chan int)
+	a := make(chan int, 5)
 	go func(i int) {
 		for {
+			time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 			a <- i
 			i++
 		}
@@ -18,7 +21,9 @@ func main() {
 	counterB := counter(100)
 	for i := 0; i < 5; i++ {
 		a := <-counterA
+		b := <-counterA
 		fmt.Printf("(A -> %d, B -> %d) ", a, <-counterB)
+		fmt.Println(b)
 	}
 	fmt.Println()
 }
