5772d002de39f252f71adc354f0f0276ac824dd4
Toomore Chiang <toomore0929@gmail.com>
2014-07-29 23:47:46 +0800

    Add test note.

--- test_struct.go
+++ test_struct.go
@@ -6,12 +6,15 @@ type Counter struct {
 	count int
 }
 
-func (counter Counter) Plus() {
-	counter.count++
+//func (counter Counter) Plus() { //count still = 0
+func (counter *Counter) Plus() { //count++
+	//counter.count++
+	counter.count += 1
 }
 
 func main() {
 	var counter Counter
 	counter.Plus()
+	//(&counter).Plus() // the same
 	fmt.Println(counter.count)
 }
