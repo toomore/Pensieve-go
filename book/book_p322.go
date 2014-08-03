6990d424fe38cc134adf031f29c6a098cbc53236
Toomore Chiang <toomore0929@gmail.com>
2014-08-03 17:59:00 +0800

    Add concurrency http get.

---
+++ book/book_p322.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func HttpGet(url string, done chan []byte, index int) (data []byte) {
+	result, _ := http.Get(url)
+	defer result.Body.Close()
+	data, _ = ioutil.ReadAll(result.Body)
+	fmt.Println(index, url)
+	done <- data
+	return
+}
+
+func main() {
+	done := make(chan []byte, 5)
+	var urls []string
+	urls = append(urls, "http://httpbin.org/get")
+	urls = append(urls, "http://google.com/")
+	urls = append(urls, "http://www.pinkoi.com")
+	urls = append(urls, "http://toomore.net/")
+	for index, url := range urls {
+		go HttpGet(url, done, index)
+	}
+	for i := 0; i < len(urls); i++ {
+		<-done
+		//fmt.Println(<-done)
+	}
+}
