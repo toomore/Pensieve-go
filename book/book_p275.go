3ee5adf5b1c4ddf16f1b7e5451a9f5634af1011c
Toomore Chiang <toomore0929@gmail.com>
2014-08-03 15:41:23 +0800

    Add anonymous fields struct.

--- book/book_p275.go
+++ book/book_p275.go
@@ -2,8 +2,24 @@ package main
 
 import "fmt"
 
+type Person struct {
+	name  string
+	title []string
+	age   int
+}
+
+type Author struct {
+	name    Person // Named fields
+	created string
+}
+
+type Author2 struct {
+	Person  // Anonymous fields
+	created string
+}
+
 func exa() {
-	points := []struct{ x, y int }{{1, 2}, {3, 4}, {5, 6}} // Anontmous struct.
+	points := []struct{ x, y int }{{1, 2}, {3, 4}, {5, 6}} // Anonymous struct.
 	for _, point := range points {
 		fmt.Printf("%d %d\n", point.x, point.y)
 	}
@@ -11,4 +27,14 @@ func exa() {
 
 func main() {
 	exa()
+	toomore := Author{Person{"Toomore Chiang", []string{"Python engineer", "Man"}, 30}, "Today"}
+	fmt.Println(toomore)
+	toomore.name.age = 31
+	fmt.Println(toomore)
+
+	fmt.Println("----- Author 2 -----")
+	toomore2 := Author2{Person{"Toomore Chiang", []string{"Python engineer", "Man"}, 30}, "Today"}
+	fmt.Println(toomore2)
+	toomore2.age = 31
+	fmt.Println(toomore2)
 }
