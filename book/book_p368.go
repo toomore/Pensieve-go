aa6f3cb6fbe16476596e9c201ce920ce2c6a6a31
Toomore Chiang <toomore0929@gmail.com>
2014-08-10 23:56:41 +0800

    Add json.Unmarshal.

--- book/book_p368.go
+++ book/book_p368.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
-	//data := "{\"name\": \"Toomore\"}"
-	data := make(map[string]string)
-	data["name"] = "Toomore"
-	fmt.Println(json.Marshal(data))
+	data := []byte(`[{"name": "Toomore"}]`)
+	//data := make(map[string]string)
+	//data["name"] = "Toomore"
+	type JsonData struct {
+		name string
+	}
+	json_data := []JsonData{}
+	json.Unmarshal(data, &json_data)
+	fmt.Printf("%+v", json_data)
 }
