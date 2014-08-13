eedfae1fd864c3f5a4ac40132e6f3b164eb4522e
Toomore Chiang <toomore0929@gmail.com>
2014-08-14 02:45:45 +0800

    Figure out time format layout.

--- test_time.go
+++ test_time.go
@@ -7,8 +7,15 @@ func main() {
 	t := time.Now()
 	u := t.Unix
 	fmt.Println(t.Unix(), u())
-	fmt.Println(t.Format("2014/09/29"))
-	value := "Thu, 05/19/11, 10:47PM"
-	pt, _ := time.Parse(value, value)
+	fmt.Println(t.Format(time.RFC3339))
+	//value := "Thu, 05/19/11, 10:47PM"
+	value := "2014/09/29 21:30:45"
+	//layout := "Mon, 01/02/06, 03:04PM"
+
+	// layout format rule
+	// http://golang.org/src/pkg/time/format.go#L117
+	layout := "2006/01/02 15:04:05"
+
+	pt, _ := time.Parse(layout, value)
 	fmt.Println(pt)
 }
