5cd2f444b93d17e1e6a9c1236030d7a9ae8ffb5d
Toomore Chiang <toomore0929@gmail.com>
2014-08-14 23:33:24 +0800

    Add json decode.

--- test_json.go
+++ test_json.go
@@ -12,7 +12,14 @@ type Person struct {
 }
 
 func main() {
+	// encode
 	data := Person{Name: "Toomore", Age: 30, Skill: "No"}
 	j, _ := json.Marshal(data)
 	fmt.Printf("%s\n", j)
+
+	// decode
+	string_data := []byte(`{"name": "Toomore", "Age": 30, "Skill": "NO"}`)
+	var json_blob Person
+	json.Unmarshal(string_data, &json_blob)
+	fmt.Printf("%+v\n", json_blob)
 }
