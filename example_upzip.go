d1e9c82e86faaf1302201e4ea35abdc8e428a1d0
Toomore Chiang <toomore0929@gmail.com>
2014-08-17 20:25:36 +0800

    Pretty string format.

--- example_upzip.go
+++ example_upzip.go
@@ -13,9 +13,14 @@ func unpackZip(filename string) error {
 	defer reader.Close()
 	for _, file := range reader.File {
 		mode := file.Mode()
-		fmt.Println(mode, file.FileInfo())
+		fmt.Printf("%v %s\t%d\t%d\t%.2f%%\n",
+			mode, file.Name, file.CompressedSize, file.UncompressedSize,
+			(float64(file.UncompressedSize)-float64(file.CompressedSize))/float64(file.UncompressedSize)*100)
 	}
 	return nil
+}
+
+func unpackZippedFile(filename string, zipFile *zip.File) {
 }
 
 func main() {
