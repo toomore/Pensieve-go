26cebc450b6e1c3ef51971d84dd5dc0443872f85
Toomore Chiang <toomore0929@gmail.com>
2014-08-17 20:46:55 +0800

    Add unzipDir.

--- example_upzip.go
+++ example_upzip.go
@@ -5,26 +5,29 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 func unpackZip(filename string) error {
+	unzipDir := filepath.Dir(filename)
 	reader, err := zip.OpenReader(filename)
 	if err != nil {
 		return err
 	}
 	defer reader.Close()
+	fmt.Printf("%+v", reader)
 	for _, file := range reader.File {
 		mode := file.Mode()
 		fmt.Printf("%v %s\t%d\t%d\t%.2f%%\n",
 			mode, file.Name, file.CompressedSize, file.UncompressedSize,
 			(float64(file.UncompressedSize)-float64(file.CompressedSize))/float64(file.UncompressedSize)*100)
-		unpackZippedFile(file)
+		unpackZippedFile(file, unzipDir)
 	}
 	return nil
 }
 
-func unpackZippedFile(zipFile *zip.File) {
-	writer, _ := os.Create(zipFile.Name)
+func unpackZippedFile(zipFile *zip.File, unzipDir string) {
+	writer, _ := os.Create(filepath.Join(unzipDir, zipFile.Name))
 	defer writer.Close()
 	reader, _ := zipFile.Open()
 	defer reader.Close()
