8844c66567438a88e1f07e34c23a19c384275ea5
Toomore Chiang <toomore0929@gmail.com>
2015-03-04 23:17:26 +0800

    Using o flag.

--- http_detect/build.bash
+++ http_detect/build.bash
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-go build
-mv ./http_detect ./http_detect_darwin_amd64
-GOOS=linux GOARCH=amd64 go build
-mv ./http_detect ./http_detect_linux_amd64
-GOOS=linux GOARCH=arm go build
-mv ./http_detect ./http_detect_linux_arm
+go build -v -o ./http_detect_darwin_amd64
+GOOS=linux GOARCH=amd64 go build -v -o ./http_detect_linux_amd64
+GOOS=linux GOARCH=arm go build -v -o ./http_detect_linux_arm
