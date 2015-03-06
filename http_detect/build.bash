bfd448fb4355fcb0854aef27320eff14d045bba9
Toomore Chiang <toomore0929@gmail.com>
2015-03-06 10:50:19 +0800

    Add arm version.

--- http_detect/build.bash
+++ http_detect/build.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 go build -v -o ./http_detect_darwin_amd64
 GOOS=linux GOARCH=amd64 go build -v -o ./http_detect_linux_amd64
-GOOS=linux GOARCH=arm go build -v -o ./http_detect_linux_arm
+GOOS=linux GOARCH=arm GOARM=6 go build -v -o ./http_detect_linux_arm
