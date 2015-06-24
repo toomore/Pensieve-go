e30173da6a333f61806550e78a73ac587dd93f3e
Toomore Chiang <toomore0929@gmail.com>
2015-06-24 15:01:46 +0800

    Add check key.

--- example_hmac/main.go
+++ example_hmac/main.go
@@ -8,8 +8,19 @@ import "golang.org/x/crypto/sha3"
 
 const hashkey = "toomore.net"
 
+func check(key, msg []byte) bool {
+	return hmac.Equal(key, hmac.New(sha3.New512, []byte(hashkey)).Sum(msg))
+}
+
 func main() {
 	h := hmac.New(sha3.New512, []byte(hashkey))
-	h.Write([]byte("Toomore is me"))
-	log.Printf("%x", h.Sum(nil))
+	msg := []byte("Toomore is me")
+	encodeKey := h.Sum(msg)
+	log.Printf("%x", encodeKey)
+
+	// true
+	log.Println(check(encodeKey, msg))
+
+	// false
+	log.Println(check([]byte(hashkey), msg))
 }
