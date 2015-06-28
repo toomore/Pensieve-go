31a40af017926a6adbc5de6b588cd7241c54802c
Toomore Chiang <toomore0929@gmail.com>
2015-06-28 18:48:06 +0800

    Simple send.

--- aws/ses/main.go
+++ aws/ses/main.go
@@ -2,21 +2,54 @@ package main
 
 import (
 	"fmt"
+	"net/mail"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/service/ses"
 )
 
 var profile = credentials.NewStaticCredentials(os.Getenv("AWSID"), os.Getenv("AWSKEY"), "")
 var config = aws.DefaultConfig
+var user = &mail.Address{
+	Name:    "講太多",
+	Address: "toomore0929@gmail.com",
+}
+
+var sender = &mail.Address{
+	Name:    "講太多",
+	Address: "me@toomore.net",
+}
 
 func init() {
 	config.Region = "us-east-1"
 	config.Credentials = profile
 }
 
+func message() *ses.SendEmailInput {
+	return &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			ToAddresses: []*string{aws.String(user.String())},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				HTML: &ses.Content{
+					Charset: aws.String("UTF-8"),
+					Data:    aws.String("Hello Toomore!"),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String("UTF-8"),
+				Data:    aws.String("Test send from aws Go SDK"),
+			},
+		},
+		Source: aws.String(sender.String()),
+	}
+}
+
 func main() {
-	//ses := ses.New(aws.)
-	fmt.Printf("%+v\n", config)
+	msg := ses.New(config)
+	fmt.Printf("%+v\n", msg)
+	fmt.Println(msg.SendEmail(message()))
 }
