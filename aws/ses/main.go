8795cdd004459911622c08bd3c80d3fe09335526
Toomore Chiang <toomore0929@gmail.com>
2015-06-30 19:17:58 +0800

    Try using simpleses.

--- aws/ses/main.go
+++ aws/ses/main.go
@@ -5,9 +5,7 @@ import (
 	"net/mail"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/toomore/Pensieve-go/aws/ses/simpleses"
 )
 
 var user = &mail.Address{
@@ -20,33 +18,36 @@ var sender = &mail.Address{
 	Address: "me@toomore.net",
 }
 
-func message() *ses.SendEmailInput {
-	return &ses.SendEmailInput{
-		Destination: &ses.Destination{
-			ToAddresses: []*string{aws.String(user.String())},
-		},
-		Message: &ses.Message{
-			Body: &ses.Body{
-				HTML: &ses.Content{
-					Charset: aws.String("UTF-8"),
-					Data:    aws.String("Hello Toomore!"),
-				},
-			},
-			Subject: &ses.Content{
-				Charset: aws.String("UTF-8"),
-				Data:    aws.String("Test send from aws Go SDK"),
-			},
-		},
-		Source: aws.String(sender.String()),
-	}
-}
+//func message() *ses.SendEmailInput {
+//	return &ses.SendEmailInput{
+//		Destination: &ses.Destination{
+//			ToAddresses: []*string{aws.String(user.String())},
+//		},
+//		Message: &ses.Message{
+//			Body: &ses.Body{
+//				HTML: &ses.Content{
+//					Charset: aws.String("UTF-8"),
+//					Data:    aws.String("Hello Toomore!"),
+//				},
+//			},
+//			Subject: &ses.Content{
+//				Charset: aws.String("UTF-8"),
+//				Data:    aws.String("Test send from aws Go SDK"),
+//			},
+//		},
+//		Source: aws.String(sender.String()),
+//	}
+//}
 
 func main() {
-	var profile = credentials.NewStaticCredentials(os.Getenv("AWSID"), os.Getenv("AWSKEY"), "")
-	var config = aws.DefaultConfig
-	config.Region = "us-east-1"
-	config.Credentials = profile
-	msg := ses.New(config)
-	fmt.Printf("%+v\n", msg)
-	fmt.Println(msg.SendEmail(message()))
+	//var profile = credentials.NewStaticCredentials(os.Getenv("AWSID"), os.Getenv("AWSKEY"), "")
+	//var config = aws.DefaultConfig
+	//config.Region = "us-east-1"
+	//config.Credentials = profile
+	//msg := ses.New(config)
+	//fmt.Printf("%+v\n", msg)
+	//fmt.Println(msg.SendEmail(message()))
+	ses := simpleses.New(os.Getenv("AWSID"), os.Getenv("AWSKEY"))
+	msg := simpleses.Message([]*mail.Address{user}, sender, "This is send from SimpleSES.", "<b>Hello Toomore~.</b>")
+	fmt.Println(ses.SendEmail(msg))
 }
