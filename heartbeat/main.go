6151b07b2271f2fe49dc964d5c3340429ecf43d1
Toomore Chiang <toomore0929@gmail.com>
2016-04-16 20:39:57 +0800

    Using interface.

--- heartbeat/main.go
+++ heartbeat/main.go
@@ -5,9 +5,28 @@ import (
 	"time"
 )
 
-func main() {
+// Jobs for run
+type Jobs interface {
+	Run()
+}
+
+func run(jobs Jobs) {
 	for {
 		fmt.Println(time.Now())
+		jobs.Run()
 		time.Sleep(time.Second)
 	}
+}
+
+type AddOne struct {
+	Name string
+}
+
+func (a *AddOne) Run() {
+	fmt.Println("1+1", a.Name)
+}
+
+func main() {
+	works := &AddOne{Name: "Add One"}
+	run(works)
 }
