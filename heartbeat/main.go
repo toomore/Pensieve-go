c838d93a114cf82efde2dd1540044ea939dd4dbe
Toomore Chiang <toomore0929@gmail.com>
2016-04-28 06:29:44 +0000

    Using chan for wait.

--- heartbeat/main.go
+++ heartbeat/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 	"time"
 )
 
@@ -10,23 +11,30 @@ type Jobs interface {
 	Run()
 }
 
-func run(jobs Jobs) {
-	for {
-		fmt.Println(time.Now())
-		jobs.Run()
-		time.Sleep(time.Second)
-	}
+func run(jobs Jobs, sleepTime time.Duration, ch chan interface{}) {
+	go func() {
+		for {
+			fmt.Println(time.Now())
+			jobs.Run()
+			time.Sleep(sleepTime)
+		}
+	}()
 }
 
+// AddOne struct
 type AddOne struct {
 	Name string
 }
 
+// Run func
 func (a *AddOne) Run() {
 	fmt.Println("1+1", a.Name)
 }
 
 func main() {
+	debug.SetGCPercent(-1)
+	endChan := make(chan interface{})
 	works := &AddOne{Name: "Add One"}
-	run(works)
+	run(works, time.Second, endChan)
+	<-endChan
 }
