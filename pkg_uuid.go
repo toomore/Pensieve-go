51b93a9ec177a3e3a773b287f5264fb7a773b1d8
Toomore Chiang <toomore0929@gmail.com>
2016-04-28 10:16:27 +0000

    Update uuid.

--- pkg_uuid.go
+++ pkg_uuid.go
@@ -1,15 +1,15 @@
 package main
 
-import "code.google.com/p/go-uuid/uuid"
+import "github.com/pborman/uuid"
 import "fmt"
 
 func main() {
-    fmt.Println("New:", uuid.New())
-    fmt.Println("NodeId:", uuid.NodeID())
-    x := uuid.NewRandom()
-    fmt.Println(x)
-    fmt.Println(uuid.Parse(x.String()))
-    fmt.Println(uuid.NewMD5(x, []byte{100}))
-    fmt.Println(x.Time())
-    fmt.Println(x.URN())
+	fmt.Println("New:", uuid.New())
+	fmt.Println("NodeId:", uuid.NodeID())
+	x := uuid.NewRandom()
+	fmt.Println(x)
+	fmt.Println(uuid.Parse(x.String()))
+	fmt.Println(uuid.NewMD5(x, []byte{100}))
+	fmt.Println(x.Time())
+	fmt.Println(x.URN())
 }
