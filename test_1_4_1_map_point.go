b7ba1467882fa87dd9f2e45cf3236cb6ad1b9e2a
Toomore Chiang <toomore0929@gmail.com>
2016-04-29 05:49:00 +0000

    Test map size limit.

--- test_1_4_1_map_point.go
+++ test_1_4_1_map_point.go
@@ -1,8 +1,15 @@
 package main
 
+import "log"
+
 func main() {
-	var m = make(map[int]int)
-	for i := 0; i < 50000000; i++ {
-		m[i] = 1
+	//var m = make(map[int]int)
+	var m = make(map[int64]int)
+	for i := 0; i < 5000000; i++ {
+		//var stats runtime.MemStats
+		//runtime.GC()
+		//runtime.ReadMemStats(&stats)
+		log.Println(stats.Alloc)
+		m[int64(i)] = 1
 	}
 }
