c64c05723b1aebdb805545aa6f7e3fdafddb5d5e
Toomore Chiang <toomore0929@gmail.com>
2016-05-20 10:30:47 +0800

    Test net/http/pprof.

--- interface/main.go
+++ interface/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 type Interface interface {
@@ -26,6 +29,7 @@ func run(data Interface) {
 }
 
 func main() {
+	log.Println(http.ListenAndServe(":6060", nil))
 	s := &Base{Name: "S****y"}
 	run(s)
 	s.call()
