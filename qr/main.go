3887265f5a0d030af48181649f25c31cc65fa298
Toomore Chiang <toomore0929@gmail.com>
2017-10-12 17:30:21 +0800

    Add more option

--- qr/main.go
+++ qr/main.go
@@ -1,30 +1,63 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"flag"
+	"fmt"
 	"image/png"
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/qr"
 )
 
+var (
+	msg          = flag.String("f", "./msg.txt", "message file")
+	size         = flag.Int("s", 600, "PNG size")
+	q            = flag.String("q", "M", "L(7%), M(15%), Q(25%), H(30%)")
+	outputbase64 = flag.Bool("b64", false, "Output base64 file")
+
+	qm = map[string]qr.ErrorCorrectionLevel{
+		"L": qr.L,
+		"M": qr.M,
+		"Q": qr.Q,
+		"H": qr.H,
+	}
+)
+
 func main() {
-	content, err := ioutil.ReadFile("./msg.txt")
+	flag.Parse()
+
+	content, err := ioutil.ReadFile(*msg)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Create the barcode
-	qrCode, _ := qr.Encode(string(content), qr.Q, qr.Unicode)
+	qrCode, _ := qr.Encode(string(content), qm[*q], qr.Unicode)
 
-	// Scale the barcode to 200x200 pixels
-	qrCode, _ = barcode.Scale(qrCode, 200, 200)
+	// Scale the barcode to size*size pixels
+	qrCode, _ = barcode.Scale(qrCode, *size, *size)
 
 	// create the output file
-	file, _ := os.Create("qrcode.png")
+	now := time.Now().Unix()
+	file, _ := os.Create(fmt.Sprintf("qrcode_%d.png", now))
 	defer file.Close()
 
 	// encode the barcode as png
 	png.Encode(file, qrCode)
+
+	// convert to base64
+	if *outputbase64 {
+		b64file, _ := os.Create(fmt.Sprintf("qrcode_%d_base64.txt", now))
+		defer b64file.Close()
+
+		buf := new(bytes.Buffer)
+		png.Encode(buf, qrCode)
+
+		b64file.Write([]byte(fmt.Sprintf("data:image/png;base64,%s", base64.StdEncoding.EncodeToString(buf.Bytes()))))
+	}
 }
